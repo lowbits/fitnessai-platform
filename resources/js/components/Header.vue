fa2048e4d9873b7f06f05ad71610e0fd2fa1bc7e
Tobias Lobitz <hallo@tobiaslobitz.de>
2026-01-21 09:41:03 +0100

    fix: Layout shift

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -10,8 +10,8 @@ const { language: selectedLanguage } = useSelectedLanguage();
         <nav class="container mx-auto px-6 py-4 md:px-0">
             <Link :href="`/${selectedLanguage}`">
                 <img
-                    width="112"
-                    height="38"
+                    width="883"
+                    height="303"
                     class="h-auto w-28"
                     src="/fytrr-logo.svg"
                     alt="fytrr.com logo"
